eaedb741e538b8428095187ad371cd62d2517997
Scripting_Entity <vbolea8@gmail.com>
2026-01-27 18:58:51 +0200

    added new gitignore file

--- CMakeFiles/fields_impl.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fields_impl.dir/fields_impl/pine_tree_forest.cpp.o"
-  "CMakeFiles/fields_impl.dir/fields_impl/pine_tree_forest.cpp.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/fields_impl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/patterns_impl.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for patterns_impl.
-# This may be replaced when dependencies are built.
