6029d52ceca0e5672f416524edc7887c318a54f0
Scripting_Entity <vbolea8@gmail.com>
2026-01-27 18:50:32 +0200

    alpha

---
+++ CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/scripting_entity/Desktop/projects/Macro Nation/application.cpp" "CMakeFiles/app.dir/application.cpp.o" "gcc" "CMakeFiles/app.dir/application.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/core.cpp" "CMakeFiles/app.dir/core.cpp.o" "gcc" "CMakeFiles/app.dir/core.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/app.dir/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/app.dir/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/imgui.cpp" "CMakeFiles/app.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/imgui.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/imgui_draw.cpp" "CMakeFiles/app.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/imgui_draw.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/imgui_tables.cpp" "CMakeFiles/app.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/imgui_tables.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/imgui/imgui_widgets.cpp" "CMakeFiles/app.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/app.dir/imgui/imgui_widgets.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/interface.cpp" "CMakeFiles/app.dir/interface.cpp.o" "gcc" "CMakeFiles/app.dir/interface.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/theme_api.cpp" "CMakeFiles/app.dir/theme_api.cpp.o" "gcc" "CMakeFiles/app.dir/theme_api.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/utils.cpp" "CMakeFiles/app.dir/utils.cpp.o" "gcc" "CMakeFiles/app.dir/utils.cpp.o.d"
+  "/home/scripting_entity/Desktop/projects/Macro Nation/walk.cpp" "CMakeFiles/app.dir/walk.cpp.o" "gcc" "CMakeFiles/app.dir/walk.cpp.o.d"
+  "" "app" "gcc" "CMakeFiles/app.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/patterns_impl.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patterns_impl.dir/patterns_impl/tidalwave.cpp.o"
+  "CMakeFiles/patterns_impl.dir/patterns_impl/tidalwave.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/patterns_impl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
